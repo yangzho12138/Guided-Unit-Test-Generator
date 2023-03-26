44ddd6b5581054f0e0c447a8b892abb82135d08d
宋熙朋 <songxipeng@Xipeng-Songs-MacBook-Pro.local>
2023-03-26 10:33:48 -0500

    Test generator

--- src/main/java/edu/illinois/cs/test/generator/TestGenerator.java
+++ src/main/java/edu/illinois/cs/test/generator/TestGenerator.java
@@ -2,6 +2,8 @@ package edu.illinois.cs.test.generator;
 
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
@@ -9,15 +11,13 @@ import com.github.javaparser.utils.SourceRoot;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
 import static com.github.javaparser.StaticJavaParser.parse;
-import static com.github.javaparser.StaticJavaParser.parseExplicitConstructorInvocationStmt;
 
 public class TestGenerator extends VoidVisitorAdapter {
 
@@ -27,6 +27,10 @@ public class TestGenerator extends VoidVisitorAdapter {
     Set<Long> longsPool;
     Set<Boolean> booleansPool;
     Set<Object> objectsPool;
+
+    List<MethodDeclaration> methods;
+    List<ConstructorDeclaration> constructors;
+
     public TestGenerator(String target) {
         integersPool = PoolInit.valuePool.integersPool;
         stringsPool = PoolInit.valuePool.stringsPool;
@@ -34,7 +38,18 @@ public class TestGenerator extends VoidVisitorAdapter {
         longsPool = PoolInit.valuePool.longsPool;
         booleansPool = PoolInit.valuePool.booleansPool;
         objectsPool = PoolInit.valuePool.objectsPool;
+        methods = new ArrayList<>();
+        constructors = new ArrayList<>();
         MethodTraverse(target);
+//        for (ConstructorDeclaration cd: constructors) {
+//            System.out.println("==============");
+//            System.out.println(cd.toString());
+//        }
+        try{
+            construct();
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void MethodTraverse(String target){
@@ -60,6 +75,8 @@ public class TestGenerator extends VoidVisitorAdapter {
     public void visit(MethodDeclaration n, Object arg) {
         super.visit(n, arg);
 
+        methods.add(n);
+
 //        for (Parameter p: n.getParameters()) {
 //            System.out.println(p);
 //        }
@@ -67,23 +84,30 @@ public class TestGenerator extends VoidVisitorAdapter {
 //            System.out.println(i);
 //        }
 //        System.out.println(this.integersPool.size());
+//
+//        // find parameter types
+//        NodeList<Parameter> parameters = n.getParameters();
+//        List<String> parametersList = new ArrayList<String>();
+//        for (Parameter p: parameters) {
+//            parametersList.add(p.getType().toString());
+//        }
+//        System.out.println(parametersList);
+////        System.out.println("+++++++++++++");
+//
+//        // create arguments from value pools
+////        Object[] arguments = new Object[parameters.size()];
+//        List<Object> arguments = new ArrayList<Object>();
+//        int argumentLength = parameters.size();
+//        invokeMethod(parametersList, 0, n, argumentLength, arguments);
+//
+//        System.out.println("-----------");
+    }
 
-        // find parameter types
-        NodeList<Parameter> parameters = n.getParameters();
-        List<String> parametersList = new ArrayList<String>();
-        for (Parameter p: parameters) {
-            parametersList.add(p.getType().toString());
-        }
-        System.out.println(parametersList);
-//        System.out.println("+++++++++++++");
+    @Override
+    public void visit(ConstructorDeclaration n, Object arg) {
+        super.visit(n, arg);
 
-        // create arguments from value pools
-//        Object[] arguments = new Object[parameters.size()];
-        List<Object> arguments = new ArrayList<Object>();
-        int argumentLength = parameters.size();
-        invokeMethod(parametersList, 0, n, argumentLength, arguments);
-
-        System.out.println("-----------");
+        constructors.add(n);
     }
 
     public void invokeMethod(List<String> parametersList, int i, MethodDeclaration n, int argumentLength, List<Object> arguments) {
@@ -149,6 +173,67 @@ public class TestGenerator extends VoidVisitorAdapter {
                 }
             }
         }
+    }
+
+    public void generateTestFile(){
+
+    }
+
+    public void construct() throws ClassNotFoundException, NoSuchMethodException {
+        for(ConstructorDeclaration cd : constructors) {
+            // 创建constructor -> 加入pool -> 生成test
+
+            if (cd.isPrivate()) {
+                continue;
+            }
+            // get the parameters of the constructor
+            NodeList<Parameter> parameters = cd.getParameters();
+            for (Parameter p: parameters) {
+                System.out.println(p.getType());
+            }
+            System.out.println(parameters);
+
+            // get the name of the constructor and find its class
+            String constructorName = cd.getNameAsString();
+            System.out.println(constructorName);
+            ClassOrInterfaceDeclaration classDeclaration = cd.findAncestor(ClassOrInterfaceDeclaration.class).get();
+            String className = classDeclaration.getFullyQualifiedName().get();
+            System.out.println(className);
+            // get the class object
+            Class<?> classObject = Class.forName(className);
+
+
+            // invoke the constructor
+
+
+//            System.out.println(constructor);
+
+            System.out.println("==============");
+        }
+
+    }
+
+    public void generateTest(){
+        Random rand = new Random();
+        int randomInt = rand.nextInt(methods.size());
+        // pick a method randomly
+        MethodDeclaration method = methods.get(randomInt);
+
+        // find parameter types
+        NodeList<Parameter> parameters = method.getParameters();
+        List<String> parametersList = new ArrayList<>();
+        for (Parameter p: parameters) {
+            parametersList.add(p.getType().toString());
+        }
+        System.out.println(parametersList);
+
+        // create arguments from value pools
+        List<Object> arguments = new ArrayList<Object>();
+        int argumentLength = parameters.size();
+        invokeMethod(parametersList, 0, method, argumentLength, arguments);
+
+        // 根据方法所在的类，去constructor list中找到对应的实体，调用该函数，生成test
+
     }
 
 }
