060de41532812b0692ed7d1b226aa1f102d9cdc5
宋熙朋 <songxipeng@Xipeng-Songs-MacBook-Pro.local>
2023-05-04 22:21:47 -0500

    directly runnable version

--- src/main/java/edu/illinois/cs/test/Application.java
+++ src/main/java/edu/illinois/cs/test/Application.java
@@ -8,9 +8,9 @@ public class Application{
     public static void main(String[] args) {
         // init the pool
         long startTime = System.currentTimeMillis();
-        PoolInit pool = new PoolInit("src/main/java/org/jsoup/select");
+        PoolInit pool = new PoolInit("src/main/java/org/jsoup/");
 
-        TestGenerator tg = new TestGenerator("src/main/java/org/jsoup/select");
+        TestGenerator tg = new TestGenerator("src/main/java/org/jsoup/");
 
         // Get end time
         long endTime = System.currentTimeMillis();
